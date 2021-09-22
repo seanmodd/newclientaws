c496d9ff1466bdad6c0325ff141d4eea707d051d
Sean <>
2021-09-21 17:27:51 -0700

    created nav

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+const { getRedirectStatus } = require('next/dist/lib/load-custom-routes');
+
 module.exports = {
-  reactStrictMode: true,
-}
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://localhost:8000/api/:path*',
+  //     },
+  //   ]
+  // },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  async getRedirectStatus() {
+    return [];
+  },
+};
+  
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import Link from 'next/link'
+import theme from '../styles/theme';
+import '../styles/globals.css';
+import {
+  ChakraProvider,
+  ColorModeProvider,
+  useColorMode,
+  Box,
+  IconButton,
+  HStack,
+} from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+    <div>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/">
+          <a>Profile</a>
+        </Link>
+        <Link href="/protected">
+          <a>Protected</a>
+        </Link>
+      </nav>
+    </div>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
